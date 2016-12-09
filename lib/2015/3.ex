b56117a4a0a9b18bc94544d31ddc8fe91cf98889
Jon Cantwell <jon.c.cantwell@gmail.com>
2016-12-08 23:23:15 -0500

    2015 day 3 part A

--- lib/2015/3.ex
+++ lib/2015/3.ex
@@ -2,9 +2,23 @@ defmodule Advent.Fifteen.Three do
 
   @input "./input/2015/3"
 
+  def visit(step, acc) do
+    [[x, y]|_] = acc
+    do_visit(step, acc, x, y)
+  end
+
+  defp do_visit("<", acc, x, y), do: [[x - 1, y]|acc]
+  defp do_visit("v", acc, x, y), do: [[x, y - 1]|acc]
+  defp do_visit(">", acc, x, y), do: [[x + 1, y]|acc]
+  defp do_visit("^", acc, x, y), do: [[x, y + 1]|acc]
+
   def a do
-    File.stream!(@input)
-    |> String.codepoints
+    File.read!(@input)
+    |> String.strip
+    |> String.graphemes
+    |> Enum.reduce([[0, 0]], &visit/2)
+    |> Enum.uniq
+    |> Enum.count
   end
 
   def b do
