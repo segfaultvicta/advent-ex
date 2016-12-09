3cc243e1af6f5b15c221db700083f56e85cc9596
Jon Cantwell <jon.c.cantwell@gmail.com>
2016-12-09 15:46:41 -0500

    Cleaned up my code a bunch, didn't use Tokeniser at all in part B

--- lib/2016/9.ex
+++ lib/2016/9.ex
@@ -29,29 +29,6 @@ defmodule Advent.Sixteen.Nine.Tokeniser do
       _ -> Chainchomp.take_chara(proc)
     end
   end
-
-  def zipbomb("", acc), do: acc
-  def zipbomb(string, acc) do
-    process_name = :lolwat
-    Chainchomp.init(string, process_name)
-    if Chainchomp.peek(process_name) == "(" do
-      Chainchomp.take_chara(process_name)
-      size = Chainchomp.take_number(process_name)
-      Chainchomp.take_chara(process_name)
-      times = Chainchomp.take_number(process_name)
-      Chainchomp.take_chara(process_name)
-      to_duplicate = Chainchomp.take_next(process_name, size)
-      |> String.duplicate(times)
-      rest = Chainchomp.rest(process_name)
-      Chainchomp.kill(process_name)
-      zipbomb to_duplicate <> rest, acc
-    else
-      Chainchomp.take_chara(process_name)
-      rest = Chainchomp.rest(process_name)
-      Chainchomp.kill(process_name)
-      zipbomb rest, 1 + acc
-    end
-  end
 end
 
 defmodule Advent.Sixteen.Nine do
@@ -64,19 +41,18 @@ defmodule Advent.Sixteen.Nine do
 
   def zipbomb(bin), do: _zipbomb(bin, 0)
 
-  def _zipbomb([], acc), do: acc
-  def _zipbomb([ "(" | rest ], acc) do
+  defp _zipbomb([], acc), do: acc
+  defp _zipbomb([ "(" | rest ], acc) do
     Chainchomp.init(to_string(rest), :zipbomber)
     {count, times, to_duplicate, remaining_string} = _chunk(:zipbomber)
     Chainchomp.kill(:zipbomber)
-    IO.puts "(#{acc}) in zipbomb, duplicating #{to_duplicate} (size #{count}) #{times} times. remaining string: #{remaining_string}"
     if String.contains? to_duplicate, "(" do
       zipbomb(String.codepoints(remaining_string)) + zipbomb(String.codepoints(to_duplicate)) * times + acc
     else
       zipbomb(String.codepoints(remaining_string)) + count * times + acc
     end
   end
-  def _zipbomb([ _ | rest], acc), do: _zipbomb rest, 1 + acc
+  defp _zipbomb([ _ | rest], acc), do: _zipbomb rest, 1 + acc
 
   defp _chunk(pid) do
     size = Chainchomp.take_number(pid)
@@ -88,23 +64,17 @@ defmodule Advent.Sixteen.Nine do
 
   def a do
     File.read!(@input)
-    #"ADVENT(2x2)AA(4x1)BBBB(8x2)(3x3)ABCY" # "ADVENTAAAABBBB(3x3)ABC(3x3)ABCY"
     |> String.strip
     |> Tokeniser.init(:base)
     slurp("", Tokeniser.remaining(:base))
     |> String.length
   end
 
-  #firsttest = "(3x3)XYZ" # 9
-  #secondtest = "X(8x2)(3x3)ABCY" # 20
-  #degenerate = "(27x12)(20x12)(13x14)(7x10)(1x12)A" # 241920
-  #offbyone = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN" # 445
   def b do
     File.read!(@input)
     |> String.strip
     |> String.codepoints
     |> zipbomb
-    #|> (Tokeniser.zipbomb 0)
   end
 
 end
