064c3ecba8ecead078349f2926ab28026207cbed
Jon Cantwell <jon.c.cantwell@gmail.com>
2016-12-18 03:19:58 -0500

    day 18

--- lib/2016/18.ex
+++ lib/2016/18.ex
@@ -11,39 +11,52 @@ defmodule Advent.Sixteen.Eighteen do
   @rowlength 100
   #@rowlength 10
 
-  def safety_dance(prev) do
-    cond do
-      prev == "^^." -> "^"
-      prev == ".^^" -> "^"
-      prev == "^.." -> "^"
-      prev == "..^" -> "^"
-      true -> "."
-    end
-  end
-
   def safe_or_trap(position, lastrow) do
-    left = if position == 0 do "." else String.slice(lastrow, position - 1, 1) end
-    centre = String.slice(lastrow, position, 1)
-    right = if position == @rowlength - 1 do "." else String.slice(lastrow, position + 1, 1) end
-    safety_dance(left <> centre <> right)
+    #U.i position, "   safe_or_trap position"
+    #U.i vec_to_string(lastrow), "    safe_or_trap lastrow"
+    left = if position == 0 do true else lastrow[position - 1] end
+    right = if position == @rowlength - 1 do true else lastrow[position + 1] end
+    #U.i left, "    left"
+    #U.i right, "    right"
+    if left == right do true else false end
   end
 
   def buildmap(input, length) do
-    Enum.reduce(1..@mapdepth-1, [input, [input]], fn(e, [lastrow, map]) ->
-      IO.puts e
-      row = Enum.reduce(0..@rowlength-1, "", fn(index, string) ->
-        string <> safe_or_trap(index, lastrow)
+    #U.i vec_to_string(input), "original input to buildmap"
+    Enum.reduce(1..@mapdepth-1, [input, input], fn(e, [lastrow, map]) ->
+      #IO.puts e
+      #U.i vec_to_string(lastrow), "lastrow"
+      row = Enum.reduce(0..@rowlength-1, [], fn(index, rowvec) ->
+        #U.i index, "  column index"
+        #U.i row_to_string(rowvec), "  rowvec at this point"
+        [safe_or_trap(index, lastrow)|rowvec]
       end)
+      |> Enum.reverse
+      |> Vector.new
+      #U.i vec_to_string(row), "row"
+      #IO.puts "\n"
       [row, [row|map]]
     end)
-    |> List.flatten
-    |> tl
+    |> U.squish([]) |> tl
     |> Enum.reverse
+  end
+
+  def vec_to_string(vec), do: Vector.to_list(vec) |> row_to_string
+  def row_to_string(list) do
+    Enum.map(list, fn(x) -> if x do "." else "^" end end) |> Enum.join
+  end
+
+  def vectorise(input) do
+    input |> to_charlist
+    |> Enum.map(fn(x) -> if x == ?. do true else false end end)
+    |> Vector.new
   end
 
   def a do
     {elapsed, result} = time do
-      map = buildmap(@input, @rowlength)
+      map = buildmap(vectorise(@input), @rowlength) |> Enum.map(&vec_to_string/1)
+      #Enum.join(map, "\n") |> IO.puts
+
       countsafe = Enum.join(map, "") |> String.codepoints
       |> Enum.reduce(0, fn(x, acc) -> if x == "." do acc+1 else acc end end)
       #displaymap = Enum.join(map, "\n")
--- lib/helpers/utility.ex
+++ lib/helpers/utility.ex
@@ -7,4 +7,13 @@ defmodule Advent.Helpers.Utility do
   def i(thing) do
     i(thing, "something")
   end
+
+  def squish(term, acc) when not is_list(term), do: [term|acc]
+  def squish([], acc), do: acc
+  def squish([h,rest], acc) do
+    [h|squish(rest, acc)]
+  end
+  def squish([h|t], acc) do
+    [h|squish(t, acc)]
+  end
 end
