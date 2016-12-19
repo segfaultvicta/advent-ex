3c817b266b9a5529bccfda547f33864b58bbb23d
Jon Cantwell <jon.c.cantwell@gmail.com>
2016-12-18 21:32:03 -0500

    there we go, day 15

--- lib/2016/15.ex
+++ lib/2016/15.ex
@@ -2,21 +2,13 @@ defmodule Advent.Sixteen.Fifteen do
   alias Advent.Helpers.Utility, as: U
   use Timing
 
-  def divstream(start, period, rowcorrection) do
-    Stream.iterate(1, &(&1 + 1))
-    |> Stream.filter(&(rem(&1,period) == 0))
-    |> Stream.map(&(&1 + (period - start)))
-    |> Stream.map(&(&1 - rowcorrection))
-    |> Stream.filter(&(&1 > 0))
-  end
-
   def fallthrough(start, period, rowcorrection, timestamp) do
     rem(((timestamp + rowcorrection) - (period - start)),period) == 0
   end
 
   def a do
     {elapsed, result} = time do
-      Enum.take_while(1..17000, fn(x) ->
+      Enum.find(1..17000, fn(x) ->
         fallthrough(5,17,1,x) and
         fallthrough(8,19,2,x) and
         fallthrough(1, 7,3,x) and
@@ -29,7 +21,16 @@ defmodule Advent.Sixteen.Fifteen do
 
   def b do
     {elapsed, result} = time do
-      # BLUH BLUH EIGHT-HOUR RUNTIMES, THEY SAID. O(N) COMPLEXITY, THEY SAID.
+      # "BLUH BLUH EIGHT-HOUR RUNTIMES", THEY SAID. "O(N) COMPLEXITY", THEY SAID.
+      Enum.find(1..5000000, fn(x) ->
+        fallthrough(5,17,1,x) and
+        fallthrough(8,19,2,x) and
+        fallthrough(1, 7,3,x) and
+        fallthrough(7,13,4,x) and
+        fallthrough(1, 5,5,x) and
+        fallthrough(0, 3,6,x) and
+        fallthrough(0,11,7,x)
+      end)
     end
   end
 
