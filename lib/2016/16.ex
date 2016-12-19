cc2677c4e27c1f7225ac2c69c4e65a29e91958a0
Jon Cantwell <jon.c.cantwell@gmail.com>
2016-12-18 23:02:50 -0500

    day 16. caught up!

--- lib/2016/16.ex
+++ lib/2016/16.ex
@@ -3,17 +3,47 @@ defmodule Advent.Sixteen.Sixteen do
   use Timing
   import DefMemo
 
-  @input "bluh"
+  @input "10001001100000001"
+  #@input "10000"
+
+  @length 272
+  #@length 20
+
+  def dragon(charlist, length) when length(charlist) < length do
+    reverse = Enum.reverse(charlist)
+    |> Enum.map(fn(x) -> if x == ?0 do ?1 else ?0 end end)
+    dragon(charlist ++ [?0] ++ reverse, length)
+  end
+
+  def dragon(charlist, length) when length(charlist) > length do
+    charlist |> to_string |> String.slice(0, length) |> to_charlist
+  end
+
+  def chunk_lookup([h,t]) do
+    if h == t do ?1 else ?0 end
+  end
+
+  # even
+  def checksum(dragon) when rem(length(dragon), 2) == 0 do
+    Enum.chunk(dragon, 2)
+    |> Enum.map(&chunk_lookup/1)
+    |> checksum
+  end
+
+  # odd
+  def checksum(dragon) when (rem(length(dragon), 2) != 0), do: dragon
 
   def a do
     {elapsed, result} = time do
-
+      dragon(@input |> to_charlist, @length)
+      |> checksum
     end
   end
 
   def b do
     {elapsed, result} = time do
-
+      dragon(@input |> to_charlist, 35651584)
+      |> checksum
     end
   end
 
